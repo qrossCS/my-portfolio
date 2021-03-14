29e78f7baadefe0f5af74b587ecb12f2eedfe2ff
qrossCS <que.ross04@gmail.com>
2021-03-14 21:05:39 +0000

    Song generation more efficient

--- portfolio/src/main/webapp/hobbies/script.js
+++ portfolio/src/main/webapp/hobbies/script.js
@@ -3,17 +3,17 @@
 function addRandomSong() {
   
     const songs =
-      ['<iframe src="https://open.spotify.com/embed/track/3YJJjQPAbDT7mGpX3WtQ9A" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>', 
-      '<iframe src="https://open.spotify.com/embed/track/4DLkSDc0HJ4i4jxjKq3Kuk" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
-      '<iframe src="https://open.spotify.com/embed/track/4nCArjFjYyIioI6NGOSonr" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
-      '<iframe src="https://open.spotify.com/embed/track/61gjdUMePfAOD5p0gnGvfv" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
-      '<iframe src="https://open.spotify.com/embed/track/2QdSb68BzZGMgCbsrFmSLc" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'];
+      ["https://open.spotify.com/embed/track/3YJJjQPAbDT7mGpX3WtQ9A", 
+      "https://open.spotify.com/embed/track/4DLkSDc0HJ4i4jxjKq3Kuk",
+      "https://open.spotify.com/embed/track/4nCArjFjYyIioI6NGOSonr",
+      "https://open.spotify.com/embed/track/61gjdUMePfAOD5p0gnGvfv",
+      "https://open.spotify.com/embed/track/2QdSb68BzZGMgCbsrFmSLc"];
 
   // Pick a random greeting.
   const song = songs[Math.floor(Math.random() * songs.length)];
 
   // Add it to the page.
   const SongContainer = document.getElementById('song-container');
-  SongContainer.innerHTML = song;
+  SongContainer.src = song;
   
 }
